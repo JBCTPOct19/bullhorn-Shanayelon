18e9a3eae6d2d33b90c824731204a0125febd188
shanayelon <shanayelon@gmail.com>
2019-10-11 14:16:55 -0400

    first Bullhorn

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Size(min=7)
+    private String Date;
+    @NotNull
+    @Size(min=5)
+    private String content;
+    @NotNull
+    @Size(min=15)
+    private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
